fa3df51721db8407272e1999c275ed1631e525da
amarthya-14 <amarthyajonnalagadda2005@gmail.com>
2025-07-08 10:00:41 +0530

    initial commit

---
+++ src/main/java/com/codecademy/tenzies_backend/LeaderBoardController.java
@@ -0,0 +1,4 @@
+package com.codecademy.tenzies_backend;
+
+public class LeaderBoardController {
+}
---
+++ src/main/java/com/codecademy/tenzies_backend/TenziesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codecademy.tenzies_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenziesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenziesBackendApplication.class, args);
+	}
+
+}
